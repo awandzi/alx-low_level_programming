2a5bccd6218887baf03e3ca740b4d490ddac80d6
awandzi <kwamenafo@gmail.com>
2023-07-17 12:48:29 -0700

    poop

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - A program that prints a line with puts function
- * Return: 0 (succee)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzl eof game");
+puts("\"programming is like building a multilingual puzzle");
 return (0);
 }
