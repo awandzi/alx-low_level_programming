e5f41fe02489f82c1f15ada1fc9d7eac42b8b98c
awandzi <kwamenafo@gmail.com>
2023-07-21 10:43:21 -0700

    polo

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - printing numbers from 0 - 9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	char c;
+
+	if (c >= 0 ; c <= 9 ;  c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
