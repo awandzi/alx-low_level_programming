071c805a42e183b89d53fda33c3df531234ce9f8
awandzi <kwamenafo@gmail.com>
2023-07-24 15:33:39 -0700

    book

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,6 +2,7 @@
 /**
  * print_array -  a function that prints n elements of an array of integers,
  * followed by a new line
+ * @a: array name
  * @n is the number of the element of the array to be printed
  * Return: a and n inputs
  */
