94c162521314cd42157f015d1a7716e45818e249
awandzi <kwamenafo@gmail.com>
2023-07-24 15:29:33 -0700

    book

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,13 @@
+#include "main.h" 
+/**
+ * puts_half - a function that prints half of a string,
+ * followed by a new line
+ * followed by a new line
+ * @s: string
+ * Return: length
+ */
+void puts_half(char *str)
+{
+
+
+}
