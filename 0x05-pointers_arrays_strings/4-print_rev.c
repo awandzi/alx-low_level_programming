71a9e8245327799223429b6f33992ac3cacd23db
awandzi <kwamenafo@gmail.com>
2023-07-24 14:07:14 -0700

    looking

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,17 +7,17 @@
  */
 void print_rev(char *s)
 {
-	int long i = 0;
+	int longi = 0;
 
 	int o;
 
 	while (*s != '\0')
 	{
-		long i++;
+		longi++;
 		s++;
 	}
 	s--;
-		for (o = long i; o > 0; o--)
+		for (o = longi; o > 0; o--)
 		{
 			_putchar(*s);
 			s--;
