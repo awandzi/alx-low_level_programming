0189749ef7aa93e235e4d7ed2a7b48ee31edd64a
awandzi <kwamenafo@gmail.com>
2023-07-25 02:29:05 -0700

    loolp

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,7 +3,7 @@
  *char *_strcpy - a function that copies the string pointed to by src,
  * including the terminating null byte (\0),
  * @dest: copy to
- *  @src: copy from
+ * @src: copy from
  * Return: string
  */
 char *_strcpy(char *dest, char *src)
@@ -11,7 +11,7 @@ char *_strcpy(char *dest, char *src)
 	int l = 0;
 	int x = 0;
 
-	while (*(src + 1) != '\0')
+	while (*(src + l) != '\0')
 	{
 		l++;
 	}
