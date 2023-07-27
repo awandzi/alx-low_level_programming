6f0b10cc21a4868e832100e4b6d3b24f2afa5938
awandzi <kwamenafo@gmail.com>
2023-07-27 12:15:42 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-		while (!(str[index] >= 'a' && str[index] < = 'z'))
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 		if (str[index - 1] == ' ' ||
 		str[index - 1] == '\t' ||
@@ -25,7 +25,7 @@ char *cap_string(char *str)
 		str[index - 1] == ')' ||
 		str[index - 1] == '{' ||
 		str[index - 1] == '}' ||
-		index == 0
+		index == 0)
 			str[index] -= 32;
 				index++;
 	}
