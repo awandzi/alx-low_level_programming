1337dc0c0618eed11ee6a9328dc87730a564dea4
awandzi <kwamenafo@gmail.com>
2023-08-29 15:25:59 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,8 +1,7 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index -  a function that inserts a new node at a
- * given position.
+ * insert_nodeint_at_index - inserts a new node in a linked list,
  * at a given position
  * @head: pointer to the first node in the list
  * @idx: index where the new node is added
